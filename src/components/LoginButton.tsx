98390812245bfd571a92a66a73f106b2fcaa912a
Cody Morrison <cody@pixel43.com>
2019-04-13 19:39:37 -0700

    Cleanup login button and not authenticated components

--- src/components/LoginButton.tsx
+++ src/components/LoginButton.tsx
@@ -4,5 +4,9 @@ import Button from "antd/lib/button/button";
 export interface LoginButtonProps {}
 
 export default function LoginButton(props: LoginButtonProps) {
-  return <Button type="primary">Login</Button>;
+  return (
+    <Button href="/.netlify/functions/auth" type="primary">
+      Login
+    </Button>
+  );
 }
--- src/components/NotAuthenticated.tsx
+++ src/components/NotAuthenticated.tsx
@@ -2,24 +2,27 @@ import React from "react";
 
 import Row from "antd/lib/row";
 import Col from "antd/lib/col";
-import Button from "antd/lib/button";
+import Empty from "antd/lib/empty";
+import Icon from "antd/lib/icon";
 import LoginButton from "./LoginButton";
 
 export interface NotAuthenticatedProps {}
 
 export default function NotAuthenticated(props: NotAuthenticatedProps) {
   return (
-    <Row align="middle" justify="center">
-      <Col>
-        Not Authenticated
-        <Button
-          onClick={() => {
-            alert("login clicked");
-          }}
+    <Row
+      align="middle"
+      justify="center"
+      style={{ minHeight: "100vh", width: "100vw" }}
+    >
+      <Col style={{ minHeight: "100vh" }}>
+        <Empty
+          image={<Icon type="github" theme="filled" style={{ fontSize: 80 }} />}
+          description={<span>Login to start viewing repos.</span>}
+          style={{ marginTop: 50 }}
         >
-          Login
-        </Button>
-        <LoginButton />
+          <LoginButton />
+        </Empty>
       </Col>
     </Row>
   );
