5a7452a002312c24c41471eefa0138ac79283a60
Cody Morrison <cody@pixel43.com>
2019-04-13 19:38:58 -0700

    setupProxy has to be .js

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,6 +1,6 @@
-import proxy from "http-proxy-middleware";
+const proxy = require("http-proxy-middleware");
 
-module.exports = function(app: any) {
+module.exports = function(app) {
   app.use(
     proxy("/.netlify/functions/", {
       target: "http://localhost:9000/",
