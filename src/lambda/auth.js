985b03e7b25759cfc5c42d024ff55502d4dfe47e
Cody Morrison <cody@pixel43.com>
2019-04-13 19:37:57 -0700

    Update lambda function to parse query string

--- src/lambda/auth.js
+++ src/lambda/auth.js
@@ -1,12 +1,24 @@
+const qs = require("qs");
+
 exports.handler = function(event, context, callback) {
-  console.log(JSON.stringify(`Event: ${JSON.stringify(event)}`));
-  console.log(JSON.stringify(`Context: ${JSON.stringify(context)}`));
+  console.log(event);
+  console.log(context);
+
+  const url = "https://github.com/login/oauth/authorize";
+  const params = {
+    client_id: "6b8c7e17e25383980bbe",
+    scope: ["user", "public_repo"],
+    redirect_uri: "http://localhost:3000/.netlify/functions/auth-callback",
+  };
+  const encodedParams = qs.stringify(params, { arrayFormat: "comma" });
+  console.log("encodedParams: ", encodedParams);
+  console.log("test");
 
   return callback(null, {
-    statusCode: 200,
-    body: JSON.stringify({
-      name: "auth",
-      nodes: [{ id: 1 }, { id: 2 }],
-    }),
+    statusCode: 301,
+    headers: {
+      Location: `${url}?${encodedParams}`,
+    },
+    body: JSON.stringify({}),
   });
 };
