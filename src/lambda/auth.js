4239b12cdc477fb41e03bc0b962f752009869ed3
Cody Morrison <cody@pixel43.com>
2019-04-19 15:55:19 -0700

    Cleanup auth lambda

--- src/lambda/auth.js
+++ src/lambda/auth.js
@@ -1,25 +1,23 @@
-const qs = require("qs");
+require("dotenv").config();
+
+const querystring = require("querystring");
 
 exports.handler = function(event, context, callback) {
-  console.log(event);
-  console.log(context);
-
   const url = "https://github.com/login/oauth/authorize";
   const params = {
-    client_id: "6b8c7e17e25383980bbe",
-    scope: ["user", "public_repo"],
-    redirect_uri: "http://localhost:3000/.netlify/functions/auth-callback",
+    client_id: process.env.GITHUB_CLIENT_ID,
+    scope: ["read:user", "user:email", "public_repo"],
+    state: 1,
   };
-  const encodedParams = qs.stringify(params, { arrayFormat: "comma" });
-  console.log("encodedParams: ", encodedParams);
-  console.log("test");
+
+  const encodedParams = querystring.stringify(params, { arrayFormat: "comma" });
 
   return callback(null, {
-    statusCode: 301,
+    statusCode: 302,
     headers: {
       Location: `${url}?${encodedParams}`,
       "Cache-Control": "no-cache",
     },
-    body: JSON.stringify({}),
+    body: "OK",
   });
 };
