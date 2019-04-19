20c54078e31fac8d4b87cc2eb1a0034f33ac2871
Cody Morrison <cody@pixel43.com>
2019-04-19 15:54:19 -0700

    Setup Github OAuth callback lambda

--- src/lambda/auth-callback.js
+++ src/lambda/auth-callback.js
@@ -1,12 +1,60 @@
-exports.handler = function(event, context, callback) {
-  console.log(JSON.stringify(`Event: event`));
-  console.log(JSON.stringify(`Context: context`));
+require("dotenv").config();
 
-  return callback(null, {
-    statusCode: 200,
-    body: JSON.stringify({
-      name: "auth-callback",
-      nodes: [{ id: 1 }, { id: 2 }],
-    }),
-  });
+const fetch = require("node-fetch").default;
+const querystring = require("querystring");
+
+const buildURL = function(host, data) {
+  if (!host) return null;
+
+  const hostPrefix = host === "localhost:3000" ? "http://" : "https://";
+  const clientSuffix = "/authorize";
+  const queryParams = data ? querystring.stringify(data) : null;
+
+  let url = hostPrefix + host + clientSuffix;
+  if (queryParams) url += `?${queryParams}`;
+
+  return url;
+};
+
+exports.handler = function(event, context, callback) {
+  const requestData = {
+    client_id: process.env.GITHUB_CLIENT_ID,
+    client_secret: process.env.GITHUB_CLIENT_SECRET,
+    code: event.queryStringParameters.code,
+    state: event.queryStringParameters.state,
+  };
+
+  fetch("https://github.com/login/oauth/access_token", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(requestData),
+  })
+    .then((response) => {
+      return response.text();
+    })
+    .then((data) => {
+      const parsedData = JSON.parse(data);
+
+      return callback(null, {
+        statusCode: 302,
+        headers: {
+          Location: buildURL(event.headers.host, parsedData),
+        },
+        body: data,
+      });
+    })
+    .catch((error) => {
+      console.error("error: ", error.type, error.name, error.message);
+
+      return callback({
+        statusCode: 302,
+        headers: {
+          Location: buildURL(event.headers.host, error),
+        },
+        body: JSON.stringify(error),
+      });
+    });
 };
