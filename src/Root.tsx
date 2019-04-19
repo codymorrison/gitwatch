be34ac57cff2786647b40da07b84c82b38771c38
Cody Morrison <cody@pixel43.com>
2019-04-19 15:51:52 -0700

    Move dependency hookups to HOC

---
+++ src/Root.tsx
@@ -0,0 +1,28 @@
+// Libraries
+import React from "react";
+import client from "./api/apolloClient";
+
+// Styles
+import "./assets/styles/index.css";
+
+// Components
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { ApolloProvider } from "react-apollo-hooks";
+
+import App from "./views/App";
+import Authorize from "./views/Authorize";
+
+export interface RootProps {}
+
+export default function Root(props: RootProps) {
+  return (
+    <BrowserRouter>
+      <ApolloProvider client={client}>
+        <Switch>
+          <Route path="/authorize" component={Authorize} />
+          <Route path="/" component={App} />
+        </Switch>
+      </ApolloProvider>
+    </BrowserRouter>
+  );
+}
--- src/index.tsx
+++ src/index.tsx
@@ -10,9 +10,9 @@ import * as serviceWorker from "./serviceWorker";
 import "./assets/styles/index.css";
 
 // Components
-import App from "./views/App";
+import Root from "./Root";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<Root />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
