029c83bf549ad54b0a2f8591d2fc1e2cff473cbf
Cody Morrison <cody@pixel43.com>
2019-04-19 15:49:44 -0700

    Add token to header if available

--- src/api/apolloClient.ts
+++ src/api/apolloClient.ts
@@ -2,6 +2,9 @@ import ApolloClient from "apollo-boost";
 
 const client = new ApolloClient({
   uri: "https://api.github.com/graphql",
+  headers: {
+    Authorization: `token ${localStorage.getItem("githubToken")}`,
+  },
 });
 
 export default client;
