d83761c6ea441b446782f6914f77087d384ed264
Cody Morrison <cody@pixel43.com>
2019-04-23 14:32:38 -0700

    Cleanup queries and fix sidebar query

--- src/containers/Sidebar.tsx
+++ src/containers/Sidebar.tsx
@@ -3,13 +3,13 @@ import React from "react";
 import { loader } from "graphql.macro";
 import { useQuery } from "react-apollo-hooks";
 
-// GraphQL
-const SEARCH_REPOS_BY_TOPIC = loader(
-  "../api/queries/searchReposByTopic.graphql"
-);
-
 // Components
 import Layout from "antd/lib/layout";
+
+// GraphQL
+const GET_PROGRAMMING_LANGUAGES = loader(
+  "../api/queries/getProgrammingLanguages.graphql"
+);
 
 export interface SidebarProps {
   collapsed: boolean;
@@ -22,14 +22,12 @@ export interface ReposByTopicData {
 }
 
 export default function Sidebar(props: SidebarProps) {
-  const { data, error, loading } = useQuery(SEARCH_REPOS_BY_TOPIC, {
-    skip: true,
-  });
+  const { data, error, loading } = useQuery(GET_PROGRAMMING_LANGUAGES);
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>{`Error! ${error.message}`}</p>;
 
-  console.info("SEARCH_REPOS_BY_TOPIC data: ", data);
+  console.info("GET_PROGRAMMING_LANGUAGES data: ", data);
 
   return (
     <Layout.Sider trigger={null} collapsible collapsed={props.collapsed}>
