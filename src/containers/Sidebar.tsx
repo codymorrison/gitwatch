84f750ebd1445795151823bf4557c4fe9a948856
Cody Morrison <cody@pixel43.com>
2019-04-30 10:58:39 -0700

    Update sidebar queries and design based on API data (WIP)

--- src/containers/Sidebar.tsx
+++ src/containers/Sidebar.tsx
@@ -2,13 +2,18 @@
 import React from "react";
 import { loader } from "graphql.macro";
 import { useQuery } from "react-apollo-hooks";
+import { awesomeCleanup } from "../helpers/stringHelper";
+import { numberWithCommas } from "../helpers/numberHelper";
+import { LightenDarkenColor } from "../helpers/cssHelper";
 
 // Components
 import Layout from "antd/lib/layout";
+import Icon from "antd/lib/icon";
+import Tag from "antd/lib/tag";
 
 // GraphQL
-const GET_PROGRAMMING_LANGUAGES = loader(
-  "../api/queries/getProgrammingLanguages.graphql"
+const SEARCH_REPOS_BY_TOPIC = loader(
+  "../api/queries/searchReposByTopic.graphql"
 );
 
 export interface SidebarProps {
@@ -22,16 +27,94 @@ export interface ReposByTopicData {
 }
 
 export default function Sidebar(props: SidebarProps) {
-  const { data, error, loading } = useQuery(GET_PROGRAMMING_LANGUAGES);
-
+  const { data, error, loading } = useQuery(SEARCH_REPOS_BY_TOPIC);
   if (loading) return <p>Loading...</p>;
   if (error) return <p>{`Error! ${error.message}`}</p>;
 
-  console.info("GET_PROGRAMMING_LANGUAGES data: ", data);
+  let languageStyles = { backgroundColor: "#fdfdfd" };
+
+  console.log("SEARCH_REPOS_BY_TOPIC data: ", data.search);
 
   return (
-    <Layout.Sider trigger={null} collapsible collapsed={props.collapsed}>
-      Sider
+    <Layout.Sider
+      trigger={null}
+      collapsible
+      collapsed={props.collapsed}
+      width="250"
+    >
+      {data.search.nodes.map((repo: any) => {
+        console.log("repo: ", repo);
+
+        if (repo.primaryLanguage && repo.primaryLanguage.color) {
+          languageStyles.backgroundColor = LightenDarkenColor(
+            repo.primaryLanguage.color,
+            0
+          );
+        }
+
+        return (
+          <div
+            key={repo.id}
+            style={{
+              backgroundColor: "#fdfdfd",
+              padding: 10,
+              borderBottom: "1px solid #ccc",
+              //...languageStyles,
+              cursor: "pointer",
+            }}
+            onClick={(evt) => {
+              console.log("url: ", repo.url);
+              console.log(props);
+            }}
+          >
+            <h4>{awesomeCleanup(repo.name)}</h4>
+            <div>
+              <small>
+                <Icon
+                  type="star"
+                  theme="twoTone"
+                  twoToneColor="#ebc600"
+                  style={{
+                    marginRight: 5,
+                    color: "#FFD700",
+                  }}
+                />
+                {numberWithCommas(repo.stargazers.totalCount)}
+              </small>
+            </div>
+            <div>
+              {repo.repositoryTopics.nodes
+                .sort((a, b) => {
+                  if (!a || !a.topic || !a.topic.starGazers) return 1;
+                  if (!b || !b.topic || !b.topic.starGazers) return 1;
+
+                  return (
+                    a.topic.starGazers.totalCount -
+                    b.topic.starGazers.totalCount
+                  );
+                })
+                .filter((resource, index) => {
+                  return (
+                    resource.topic.name !== "awesome" &&
+                    resource.topic.name !== "awesome-list" &&
+                    resource.topic.name !== "lists" &&
+                    resource.topic.name !== "list" &&
+                    resource.topic.name !== "awesomeness" &&
+                    index <= 2
+                  );
+                })
+
+                .map((resource) => {
+                  return (
+                    <Tag key={resource.id} style={{ marginTop: 5 }}>
+                      {resource.topic.name}
+                    </Tag>
+                  );
+                })}
+            </div>
+          </div>
+        );
+      })}
     </Layout.Sider>
   );
 }
