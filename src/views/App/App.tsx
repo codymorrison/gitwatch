0d670b6e6e86b2d08697d2865db968ca8a1362e0
Cody Morrison <cody@pixel43.com>
2019-05-01 15:32:08 -0700

    Cleanup App

--- src/views/App/App.tsx
+++ src/views/App/App.tsx
@@ -3,21 +3,28 @@ import React, { useState } from "react";
 import useLocalStorage from "react-use/lib/useLocalStorage";
 
 // Components
+import { Route } from "react-router-dom";
 import Layout from "antd/lib/layout";
 import Sidebar from "../../containers/Sidebar";
 import RepoView from "../../containers/RepoView";
-import NotAuthenticated from "../../components/NotAuthenticated";
+import { RouteComponentProps } from "react-router-dom";
 
 export interface AppProps {}
 
-export default function App(props: AppProps) {
-  const [collapsed, setCollapsed] = useState(false);
-  const [githubToken, setGithubToken] = useLocalStorage("githubToken", "");
+export default function App(props: RouteComponentProps) {
+  const [collapsed, _setCollapsed] = useState(false);
+  const [githubToken, _setGithubToken] = useLocalStorage("githubToken", "");
 
   return (
     <Layout className="App" style={{ minHeight: "100vh", minWidth: "100vw" }}>
-      <Sidebar collapsed={collapsed} />
-      {githubToken ? <RepoView /> : <NotAuthenticated />}
+      <Sidebar
+        collapsed={collapsed}
+        history={props.history}
+        location={props.location}
+        match={props.match}
+      />
+
+      <Route path="/list/:owner/:repo" component={RepoView} />
     </Layout>
   );
 }
