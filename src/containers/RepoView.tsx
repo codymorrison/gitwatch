395054b1487ebc0b56d1ad97a7acaf45d25fa27a
Cody Morrison <cody@pixel43.com>
2019-05-07 15:29:06 -0700

    Add fetch for readme on RepoView. Needs cleanup.
    There's no simple readme fetch in the GraphQL API. It's also case sensitive causing issues between repos that name their file Readme.md vs readme.md.

--- src/containers/RepoView.tsx
+++ src/containers/RepoView.tsx
@@ -1,17 +1,80 @@
 // Libraries
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { parseMarkdown } from "../helpers/markdownHelper";
 
 // Components
+import { RouteComponentProps } from "react-router-dom";
 import Layout from "antd/lib/layout";
 
-export interface RepoViewProps {}
+export interface ListRepoRouteParams {
+  owner: string;
+  repo: string;
+}
 
-export default function RepoView(props: RepoViewProps) {
+export interface ReadmeProps {
+  name: string;
+  path: string;
+  sha: string;
+  size: number;
+  url: string;
+  html_url: string;
+  git_url: string;
+  download_url: string;
+  type: string;
+  content: string;
+  encoding: string;
+  _links: Object;
+}
+
+export default function RepoView(
+  props: RouteComponentProps<ListRepoRouteParams>
+) {
+  const [readme, setReadme] = useState<ReadmeProps | undefined>(undefined);
+
+  console.log("owner: ", props.match.params.owner);
+  console.log("repo: ", props.match.params.repo);
+
+  useEffect(() => {
+    const token = localStorage.getItem("githubToken");
+    const authorizeToken = token && token.length > 0 ? JSON.parse(token) : "";
+
+    const readmeFetch = async () => {
+      const data: ReadmeProps = await fetch(
+        `https://api.github.com/repos/${props.match.params.owner}/${
+          props.match.params.repo
+        }/readme`,
+        {
+          headers: {
+            Authorization: "token " + authorizeToken,
+          },
+        }
+      )
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          return data;
+        })
+        .catch((error) => {
+          console.log("error: ", error);
+          return error;
+        });
+
+      setReadme(data);
+    };
+    readmeFetch();
+  }, [props.match.params.owner, props.match.params.repo]);
+
+  console.log("readme: ", readme);
+
+  if (readme && readme.content && readme.content.length > 0)
+    console.log(parseMarkdown(readme.content, { base64: true }));
+
   return (
     <Layout>
-      <Layout.Header>Header</Layout.Header>
-
-      <Layout.Content>Content</Layout.Content>
+      <Layout.Content>
+        {readme && readme.content ? readme.content : ""}
+      </Layout.Content>
 
       <Layout.Footer>Footer</Layout.Footer>
     </Layout>
